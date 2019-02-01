2e75e77636e28b4605ca0ede376108193781b43c
etienne-napoleone <etienne@tomochain.com>
2019-02-01 11:32:12 +0700

    tests: add test for valid yml output

--- tests/test_create_tomochain_masternode.py
+++ tests/test_create_tomochain_masternode.py
@@ -1,5 +1,33 @@
+import yaml
+
+import pytest
+
 from create_tomochain_masternode import __version__
+from create_tomochain_masternode import main
+
+
+@pytest.fixture
+def runner():
+    from click.testing import CliRunner
+    runner = CliRunner()
+    return runner
 
 
 def test_version():
     assert __version__ == '0.3.0-beta'
+
+
+def test_produced_yml(runner):
+    input = (
+        "123456123456\n"
+        "123456123456\n"
+        "\n"
+        "/tmp\n"
+        "\n"
+        "\n"
+        "\n"
+    )
+    with runner.isolated_filesystem():
+        runner.invoke(main.entrypoint, ['test-node'], input=input)
+        with open('test-node/docker-compose.yml') as file:
+            assert yaml.load(file)
